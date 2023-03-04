05a2e09458c068b6211969163c66819ce92ffea6
Lukasz Smiechowski <lukasz.smiechowski@me.com>
2023-03-04 15:50:53 +0100

    implementing js to messageBtn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//nav bar logic
 const navBtns = document.querySelectorAll('.side-nav__item');
 const navContainer = document.querySelector('.side-nav');
 
@@ -20,3 +21,36 @@ navContainer.addEventListener('click', function(e) {
     .querySelector(`.${clicked.dataset.tab}`)
     .classList.add('description-active');
 });
+
+//message logic
+const meessageWindow = document.querySelector('.message')
+const btnOpenMessage = document.querySelector('.user-nav__message-btn');
+const btnCloseMessage = document.querySelector('.message__box-close');
+
+const openMessage = function (e) {
+    e.preventDefault();
+    meessageWindow.classList.remove('hidden');
+}
+
+const closeMessage = function () {
+    meessageWindow.classList.add('hidden');
+}
+
+
+btnOpenMessage.addEventListener('click', openMessage);
+btnCloseMessage.addEventListener('click', closeMessage);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !meessageWindow.classList.contains('hidden')) {
+        closeMessage();
+    }
+  });
+
+meessageWindow.addEventListener('click', function(e) {
+    
+    const clicked = e.target.closest('.message__container');
+    if (!clicked) {
+        closeMessage();
+    }
+    
+})
